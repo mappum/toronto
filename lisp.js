e7d1a327e72bac64a93cb6c804d19c7a515439a0
Matt Bell <mappum@gmail.com>
2017-07-24 12:19:35 -0400

    Added a bunch of macros

--- lisp.js
+++ lisp.js
@@ -12,7 +12,6 @@ function parse (code) {
   }
 
   function readToken () {
-    i -= 1 // re-emit first char
     let start = i
     let length = 0
     for (let char of chars()) {
@@ -39,6 +38,7 @@ function parse (code) {
       // end of token, no-op
     } else {
       // token separator
+      i -= 1 // re-emit token's first char
       peek().push(readToken())
     }
   }
@@ -49,6 +49,24 @@ function parse (code) {
   return tree
 }
 
+function * range (min = 0, max) {
+  if (max == null) {
+    max = min
+    min = 0
+  }
+  for (let i = min; i < max; i++) {
+    yield i
+  }
+}
+
+function mapIterator (func, iterator) {
+  let output = []
+  for (let value of iterator) {
+    output.push(func(value))
+  }
+  return output
+}
+
 const defaultMacros = {
   '+' (...args) {
     return '(' + args.join(' + ') + ')'
@@ -57,27 +75,112 @@ const defaultMacros = {
     args.unshift('0') // handle single args (`(- 5)` should be -5)
     return '(' + args.join(' - ') + ')'
   },
+  '%' (a, b) {
+    return `(${a} % ${b})`
+  },
   'foo' (a, b, c, d) {
     return `console.log(${a} + ${b}, ${c} + ${d})`
+  },
+  'map' (func, iterator) {
+    return `mapIterator(${func}, ${iterator})`
+  },
+  '=>' (arg, expression) {
+    return `(${arg}) => (${expression})`
+  },
+  'range' (...args) {
+    return `range(${args.join(', ')})`
+  },
+  '?' (cond, a, b = undefined) {
+    return `(${cond} ? ${a} : ${b})`
+  },
+  '!' (expression) {
+    return `!(${expression})`
+  },
+  'print' (...args) {
+    return `(console.log(${args.join(', ')}))`
+  },
+  'cond' (...args) {
+    let output = ''
+    let hasElse = args.length % 2 === 1
+    let elseValue = hasElse ? args[args.length - 1] : 'undefined'
+    let last = true
+    for (let i = args.length - 2 - (hasElse ? 1 : 0); i >= 0; i -= 2) {
+      let condition = args[i]
+      let value = args[i + 1]
+      output = `(${condition} ? ${value} : ${last ? elseValue : output})`
+      last = false
+    }
+    return output
+  },
+  'or' (a, b) {
+    return `(${a} || ${b})`
+  },
+  ',' (...args) {
+    return `${args.join(', ')}`
+  },
+  'func' (args, body) {
+    return `(function (${args}) { return (${body}) })`
   }
 }
 
 function transform (tree, macros = defaultMacros) {
   let [ operator, ...expressions ] = tree
-  let args = expressions.map((expression) => {
-    if (Array.isArray(expression)) {
-      return transform(expression, macros)
+  let args = []
+  for (let expression of expressions) {
+    if (!Array.isArray(expression)) {
+      // static token
+      args.push(expression)
+    } else {
+      // transformable expression
+      args.push(transform(expression, macros))
     }
-    return expression
-  })
+  }
   let macro = macros[operator]
+  if (!macro) throw Error(`macro "${operator}" not found`)
   return macro(...args)
 }
 
-let tree = parse(`
-  (foo 1 2
-    (+ 3 4 (- 5))
-    (+ 1 2))
+function lisp (code, macros = defaultMacros) {
+  let tree = parse(code)
+  let js = transform(tree, macros)
+  return eval(js)
+}
+
+// let tree = parse(`
+//   (foo 1 2
+//     (+ 3 4 (- 5))
+//     (+ 1 2))
+// `)
+// let tree = parse(`
+//   (map
+//     (=> i
+//       (print
+//         (? (% i 15)
+//         (? (% i 3)
+//         (? (% i 5) i 'buzz') 'fizz') 'fizzbuzz')))
+//     (range 1 101))
+// `)
+// let tree = parse(`
+//   (map
+//     (=> i
+//       (print (cond
+//         (! (% i 15)) 'fizzbuzz'
+//         (! (% i 3)) 'fizz'
+//         (! (% i 5)) 'buzz'
+//         i)))
+//     (range 1 101))
+// `)
+// let tree = parse(`
+//   (map
+//     (=> i
+//       (print (or (+
+//         (? (% i 3) '' 'fizz')
+//         (? (% i 5) '' 'buzz'))
+//         i)))
+//     (range 1 101))
+// `)
+let add = lisp(`
+  (func (, a b) (+ a b))
 `)
-console.log(JSON.stringify(tree, null, '  '))
-console.log(transform(tree))
+
+console.log(add(10, 5))
