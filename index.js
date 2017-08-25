77ff9913623fa9c5b7e4ef1425080b6f4014efd7
Matt Bell <mappum@gmail.com>
2017-08-25 09:38:13 -0700

    Ensure evalled result gets returned in evalExpansion

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function evalExpansion (code, ops, tokenizers = defaultTokenizers) {
 
   let tree = parse(code, tokenizers)
   let js = expand.call(ops, tree, ops)
-  return new Function(js).call(ops)
+  return new Function(`return (${js})`).call(ops)
 }
 
 function attachMethods (obj, ctx) {
